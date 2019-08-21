0284442b5360c3b33045d09d185b47c7f47b81b2
Alfamal <hualaier1314@163.com>
2019-08-22 01:40:04 +0800

    kaifa

---
+++ src/main/java/com/hle/spv/SpvApplication.java
@@ -0,0 +1,13 @@
+package com.hle.spv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpvApplication.class, args);
+	}
+
+}
